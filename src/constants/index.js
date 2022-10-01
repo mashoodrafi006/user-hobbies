36377c0c4f098faa7493117d64c3978944319207
Mashood Rafi <mashood.rafi@careem.com>
2022-10-01 23:46:54 +0500

    Add response codes & messages to API

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+export const API_STATUS_CODES = {
+    SUCCESS: 200,
+    NOT_FOUND: 404,
+    AUTHORIZATION_FAILED: 401,
+    ERROR_CODE: 400,
+    INTENAL_SERVER_ERROR: 500,
+    DUPLICATE_ENTRY: 11000,
+};
+
+export const RESPONSE_MESSAGES = {
+    SUCCESS: 'Sucess',
+    REQUEST_FAILED: "Request failed."
+};
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import userService from "../services/userService";
+import { API_STATUS_CODES, RESPONSE_MESSAGES } from "../constants";
 const userController = {};
 
 userController.createUser = async (req, res) => {
@@ -6,13 +7,13 @@ userController.createUser = async (req, res) => {
         const { name } = req.body;
         const userId = await userService.createUser(name);
 
-        return res.json({ status: 200, message: "User created. ", userId });
+        return res.json({ status: API_STATUS_CODES.SUCCESS, message: RESPONSE_MESSAGES.SUCCESS, userId });
     } catch (error) {
         if (error.code === 11000) {
-            return res.json({ status: 502, message: "duplicate" });
+            return res.json({ status: API_STATUS_CODES.DUPLICATE_ENTRY, message: RESPONSE_MESSAGES.REQUEST_FAILED, userId: null });
         }
 
-        return res.json({ status: 502, message: "Error " });
+        return res.json({ status: API_STATUS_CODES.INTENAL_SERVER_ERROR, message: RESPONSE_MESSAGES.REQUEST_FAILED, userId: null });
     }
 }
 
